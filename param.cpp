14682b48b3c652c5818c6c9d141f47c3017d7b3d
RahjeeManuel <ram88@students.uwf.edu>
2021-09-08 00:11:54 -0500

    param class created

--- param.cpp
+++ param.cpp
@@ -1,1 +1,12 @@
 //class to hold parsed input data
+#include "param.hpp"
+using std::cout;
+using std::endl;
+void Param::printParams() {
+    cout << "InputRedirect: [" << ((inputRedirect != NULL) ? inputRedirect : "NULL") << "]" << endl <<
+        "OutputRedirect: [" << ((outputRedirect != NULL) ? outputRedirect : "NULL") << "]" << endl <<
+        "Background: [" << background << "]" << endl <<
+        "ArgumentCount: [" << argumentCount << "]" << endl;
+    for (int i = 0; i < argumentCount; i++)
+        cout << "ArgumentVector[" << i << "]: [" << argumentVector[i] << "]" << endl;
+}
---
+++ param.hpp
@@ -0,0 +1,16 @@
+#ifndef PARAM_HPP
+#define PARAM_HPP
+#define MAXARGS 32
+#include <iostream>
+class Param
+{
+    private:
+        char *inputRedirect;            /* file name or NULL */
+        char *outputRedirect;           /* file name or NULL */
+        int background;                 /* either 0 (false) or 1 (true) */
+        int argumentCount;              /* number of tokens in argument vector */
+        char *argumentVector[MAXARGS];  /* array of strings */
+    public:
+        void printParams();
+};
+#endif
