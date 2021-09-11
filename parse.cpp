571bffe499255af3734a1aefaa59eeb80ac7f2dd
or8 <or8@cs-ssh.argo.uwf.edu>
2021-09-11 14:24:29 -0500

    parse function

--- parse.cpp
+++ parse.cpp
@@ -1,1 +1,15 @@
 //implements the command line parser of the shell
+#include"parse.hpp"
+#include<iostream>
+#include<sstream>
+#include<stdio.h>
+#include<string.h>
+
+
+using namespace std;
+
+//string line stored in char array on myshell
+Parse::Parse(char *strArray){
+    char *p;
+    p = strtok(strArray, "%s%t\n");
+}
--- parse.hpp
+++ parse.hpp
@@ -1,1 +1,12 @@
 //defines the functionality you want to expose
+#ifndef PARSE_HPP
+#define PARSE_HPP
+#include<string>
+
+class Parse{
+    private:
+        
+    public:
+        Parse(char *strArray);
+};
+#endif
